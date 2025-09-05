e57ade27cfa6bb3d405270517b2dd8f2753c2768
Benson Michael Owen <lordkz19@gmail.com>
2025-09-05 16:32:39 +0100

    submision session remove dfrom logs

--- app.js
+++ app.js
@@ -70,8 +70,8 @@ app.use(express.json());
 
 // Debug middleware (remove in production)
 app.use((req, res, next) => {
-  console.log('Session ID:', req.sessionID);
-  // console.log('User in session:', req.session.user);
+  // console.log('Session ID:', req.sessionID);
+  // console.log('User in session:', req.session.user.email);
   next();
 });
 
